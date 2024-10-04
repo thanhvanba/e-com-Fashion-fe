931f5a0688c128ccaf7d3441b42723914fe944cc
Trịnh Hoàng Phúc <hoangphuc01022002@gmail.com>
2024-10-04 19:11:38 +0700

    update: install ant design and flow-bite

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+export default function Products() {
+  return (
+    <>
+      <a href="/">
+        home
+      </a>
+
+      <a href="/products/1">
+        product 1
+      </a>
+    </>
+  );
+}
